ecbcb3b751d520011740b36945fbc9090ac8a5f9
chintan0539 <chintanpatel0539@gmail.com>
2021-05-26 21:38:02 +0530

    weather app

--- Weather App/main.py
+++ Weather App/main.py
@@ -1,10 +1,14 @@
 import requests
 
+from twilio.rest import Client
+import os
+
 api_endpoint = 'https://api.openweathermap.org/data/2.5/onecall'
 api_key = '9dedff07518b32321d272593d1207321'
-MY_LAT = 16.7028412
-MY_LOG = 74.2405329
-
+MY_LAT = 22.373022
+MY_LOG = 85.197174
+account_sid = 'AC6f8d019d05c76a24871a4753f2b34999'
+auth_token = '2e32cc9c765280232d824448241b042a'
 parameters = {
     'lat': MY_LAT,
     'lon': MY_LOG,
@@ -16,9 +20,19 @@ parameters = {
 response = requests.get(api_endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()['hourly'][:12]
-
+will_it_rain = False
 for hour in weather_data:
-    weather_id=hour['weather'][0]['id']
-    if(weather_id<700):
-        print('bring your umberalla')
-        break
+    weather_id = hour['weather'][0]['id']
+    if weather_id < 700:
+        will_it_rain = True
+
+if will_it_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It is going to rain today plz remember to get your Umbrella ☂️",
+        from_='+12023357029',
+        to='+917769938811'
+    )
+
+    print(message.status)
